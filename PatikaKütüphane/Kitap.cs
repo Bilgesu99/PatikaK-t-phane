57336d8a0f660ce329a4a793348bbfccf3affb76
asus <asus@DESKTOP-OTTG4TB>
2024-08-18 18:38:53 +0300

    Add project files.

---
+++ PatikaKütüphane/Kitap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaKütüphane
+{
+    public class Kitap  //İlk olarak kitabın özelliklerini atıyoruz.
+    {
+        public string Ad { get; set; }
+        public string YazarAd { get; set; }
+        public string YazarSoyad { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayınevi { get; set; }
+        public DateTime KayitTarihi { get; private set; }
+
+        // Default constructor
+        public Kitap()
+        {
+            KayitTarihi = DateTime.Now; // Kayıt tarihi otomatik olarak atanır
+        }
+
+        // Parametreli constructor
+        public Kitap(string ad, string yazarAd, string yazarSoyad, int sayfaSayisi, string yayınevi)
+        {
+            Ad = ad;
+            YazarAd = yazarAd;
+            YazarSoyad = yazarSoyad;
+            SayfaSayisi = sayfaSayisi;
+            Yayınevi = yayınevi;
+            KayitTarihi = DateTime.Now; // Kayıt tarihi otomatik olarak atanır
+
+        }
+
+        // Kitap bilgilerini ekrana yazdırma metodu
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}");
+            Console.WriteLine($"Yazar: {YazarAd} {YazarSoyad}");
+            Console.WriteLine($"Sayfa Sayısı: {SayfaSayisi}");
+            Console.WriteLine($"Yayınevi: {Yayınevi}");
+            Console.WriteLine($"Kayıt Tarihi: {KayitTarihi}");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+        
+    
---
+++ PatikaKütüphane/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using PatikaKütüphane;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Parametreli constructor kullanarak kitap oluşturma
+        Kitap kitap1 = new Kitap("Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+        kitap1.BilgileriYazdir();
+
+        // Default constructor kullanarak kitap oluşturma
+        Kitap kitap2 = new Kitap();
+        kitap2.Ad = "Bir Kitap";
+        kitap2.YazarAd = "Ahmet";
+        kitap2.YazarSoyad = "YILMAZ";
+        kitap2.SayfaSayisi = 320;
+        kitap2.Yayınevi = "Alfa Yayınları";
+        kitap2.BilgileriYazdir();
+    }
+}
